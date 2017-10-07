97ee283357bade1d524e9ebf2968f9fd371095e5
Charles Brands <charles.brands@gmail.com>
2017-10-07 11:17:05 +0200

    Added buttons (not yet functional)

--- src/components/Mainview.js
+++ src/components/Mainview.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { fetchCategories, fetchPosts, fetchPostsForCategory, selectCategory } from "../actions/index";
 import PostList from './PostList';
 import CategoryList from "./CategoryList";
+import '../styles/app.css';
 
 class Mainview extends Component {
     componentDidMount() {
@@ -21,19 +22,13 @@ class Mainview extends Component {
     }
 
     getPosts = () => {
-        if (this.isEmpty(this.props.selectedCategory)) {
-            const pathName = this.props.location.pathname;
-            if (pathName === "/") {
-                this.props.selectCategory("home");
-                this.props.fetchPosts();
-            } else {
-                this.props.selectCategory(pathName.substr(1,));
-                this.props.fetchPostsForCategory(pathName.substr(1,));
-            }
-        } else if (this.props.selectedCategory === 'home') {
+        const pathName = this.props.location.pathname;
+        if (pathName === "/") {
+            this.props.selectCategory("home");
             this.props.fetchPosts();
         } else {
-            this.props.fetchPostsForCategory(this.props.selectedCategory);
+            this.props.selectCategory(pathName.substr(1,));
+            this.props.fetchPostsForCategory(pathName.substr(1,));
         }
     }
 
@@ -55,7 +50,15 @@ class Mainview extends Component {
                     </Link>
                 </aside>
                 <section className="col-md-8 col-xs-12">
-                    <h2>Posts</h2>
+                    <div className="margin-bottom10">
+                        <h2 className="text-center">Posts</h2>
+                        <button className="btn btn-primary margin-right10">
+                            New Post <i className="fa fa-plus" aria-hidden="true"></i>
+                        </button>
+                        <button className="btn btn-primary">
+                            Sort by <i className="fa fa-sort" aria-hidden="true"></i>
+                        </button>
+                    </div>
                     <PostList/>
                 </section>
             </div>
--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -3,10 +3,28 @@ import { Link } from "react-router-dom";
 
 const PostListItem = ({post}) => {
     return(
-        <li className="list-group-item" key={post.id}>
-            <Link to={`/${post.category}/${post.id}`}>
-                {post.title}
-            </Link>
+        <li className="list-group-item clearfix" key={post.id}>
+            <div className="margin-bottom10">
+                <Link to={`/${post.category}/${post.id}`}>
+                    {post.title}
+                    {console.log('post', post)}
+                </Link>
+                <br/>
+                {post.body}
+                <br/>
+                {post.author}
+            </div>
+            <div className="edit-buttons">
+                <button className="btn btn-primary margin-right10"><i className="fa fa-pencil" aria-hidden="true"></i></button>
+                <button className="btn btn-danger"><i className="fa fa-trash-o" aria-hidden="true"></i></button>
+                <span className="comments"><i className="fa fa-comments" aria-hidden="true"></i><span className="comments-distance">12</span></span>
+            </div>
+
+            <div className="vote-buttons">
+                <button className="btn btn-primary margin-right10"><i className="fa fa-thumbs-down" aria-hidden="true"></i></button>
+                <button className="btn btn-primary margin-right10"><i className="fa fa-thumbs-up" aria-hidden="true"></i></button>
+                <span>{post.voteScore}</span>
+            </div>
         </li>
     );
 }
